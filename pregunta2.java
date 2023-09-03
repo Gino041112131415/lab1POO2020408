3af8e7b9047cc203feb1c2188e26ba47f26c91a4
Gino041112131415 <143566661+Gino041112131415@users.noreply.github.com>
2023-09-02 21:51:48 -0500

    Create pregunta2.java

---
+++ pregunta2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	  int numero;
+	  Scanner leer = new Scanner(System.in);
+	  
+	  System.out.println("ingrese le numero ");
+	  numero = leer.nextInt();
+	  if(numero >0 && numero%2 !=0){
+	      System.out.println("true");
+	  }
+	  
+	        
+	    
+	}
+}
